52255fbdc9a818f7e3208b5b8ec803448506c1c6
louishsu <is.louishsu@foxmail.com>
2019-11-10 11:32:31 +0800

    Time: 2019-11-10 11:32:31

--- mtcnn_py/config.py
+++ mtcnn_py/config.py
@@ -5,7 +5,7 @@
 @Author: louishsu
 @E-mail: is.louishsu@foxmail.com
 @Date: 2019-10-26 10:43:04
-@LastEditTime: 2019-11-08 09:10:37
+@LastEditTime: 2019-11-10 11:26:00
 @Update: 
 '''
 from easydict import EasyDict
@@ -17,7 +17,7 @@ configer.logdir = './logs/'
 configer.ckptdir = './ckptdir/'
 
 configer.batchsize = 512
-configer.n_epoch = 50
+configer.n_epoch = 80
 
 configer.lrbase = 1e-2
 # configer.adjstep = [12, 16]
